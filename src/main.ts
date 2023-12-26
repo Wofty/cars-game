9225ddbec75c7a85001a8f02b02229872d1efeec
wofty <sorin.mihalachee@gmail.com>
2023-12-26 18:13:23 +0000

    added some tests

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
+let canvas = document.getElementById('canvas') as HTMLCanvasElement;
 
+let ctx = canvas.getContext('2d');
+
+if (ctx) {
+    ctx.fillRect(10, 100, 100, 100);
+    ctx.fillRect(10, 10, 100, 100);
+}
